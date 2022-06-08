f4a3c978bf3b1471092e9c712be4a5bba38065da
1391020381 <1391020381@qq.com>
2022-06-08 21:25:17 +0800

    动画

---
+++ vue3-intro/src/pages/Rate.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+    import { defineProps,computed } from 'vue';
+    let props = defineProps({
+        value:Number,
+        theme:{
+            thpe:String,
+            default:'orange'
+        }
+    })
+    console.log(props)
+
+ let rate = computed(()=>{
+     return "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value)
+ })
+const themeObj = {  
+    'black': '#00', 
+     'white': '#fff',  
+     'red': '#f5222d', 
+      'orange': '#fa541c', 
+       'yellow': '#fadb14', 
+        'green': '#73d13d',  
+        'blue': '#40a9ff',
+        }
+      const fontstyle = computed(()=>{
+          return `color:${themeObj[props.theme]}`
+      })  
+</script>
+
+<style>
+
+</style>
