7acb0ced8501b9cab1d608339b393f0431a88b19
1391020381 <1391020381@qq.com>
2022-06-24 17:04:22 +0800

    vue3-intro

--- vue3-intro/src/pages/about.vue
+++ vue3-intro/src/pages/about.vue
@@ -1,12 +1,14 @@
 
 <template>
     <h1>这是关于页面</h1>
+    <Count></Count>
     <button @click="toggle">click</button>
     <transition name="fade">
         <h1 v-if="showTitle">你好 Vue 3</h1>
     </transition>
 </template>
 <script setup>
+import Count from '../components/Count.vue'
 import { ref } from 'vue'
 let showTitle = ref(true)
 function toggle(){
